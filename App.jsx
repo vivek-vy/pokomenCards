e7c7aa728c424e2762eab0c59e51913bab8cade6
vivek-vy <160044844+vivek-vy@users.noreply.github.com>
2024-11-25 00:52:30 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { PokemonProject } from "./pokemon_project/1-pokemon.";
+
+export const App = () => {
+
+
+  return (
+
+    <>
+      <PokemonProject />
+    </>
+  );
+};
